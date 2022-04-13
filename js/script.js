97655374ee46f9039c61147fdb8eb3c0b8685eee
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-04-13 23:57:17 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@
 
 function Calculate_volume() {
   // input
-  let Side_a = parseInt( (document.getElementById('Side-a-here').value));
+  let Side_a = parseFloat( (document.getElementById('Side-a-here').value));
+
+  let Side_b = parseFloat( (document.getElementById('Side-b-here').value));
   
-  let Height = parseInt( (document.getElementById('Height-here').value));
+  let Height = parseFloat( (document.getElementById('Height-here').value));
   
   // naming constants and stuff
-  const Third_rate = 0.3
+  const Sixth_rate = 0.1666666667
   
-  let Pyramid_volume = Side_a * Side_b * Third_rate * Height;
+  let Pyramid_volume = Sixth_rate * Side_a * Side_b * Height;
   // remember to use .toFixed(2) to round to two decimal places
   
   // output for volume
@@ -18,6 +20,12 @@ function Calculate_volume() {
 
 
   // unused code
-  // let Side_b = parseInt( (document.getElementById('Side-b-here').value));
+
+  // parseInt
+  // document.getElementById('Pyramid-results').innerHTML = "Your volume rounded to the nearest hundreth is " + Pyramid_volume.toFixed(2) + "cm."
+  // let Pyramid_volume = Side_a * Side_b * Height * Sixth_rate;
+  // let Pyramid_volume = Side_a * Side_b * Third_rate * Height;
+  // const Third_rate = 0.3
+  
 
 }
